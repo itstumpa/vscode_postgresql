01b1a7bc12eefb6c27825b79c340393c1ac0844c
itstumpa <tumpadas1fiverr@gmail.com>
2025-12-30 16:48:46 +0600

    delete, find

--- create.sql
+++ create.sql
@@ -24,6 +24,43 @@ VALUES
 (4, 'Raj', 'Sharma', 'Raj.sharma@example.com', 'frrrrrrrrr rr rr rr', 'IT', 50000.00, '2020-01-15'),
 (5, 'Sumpa', 'Sharma', 'Sumpa.sharma@example.com', 'frrrrrrrrr rr rr rr', 'IT', 50000.00, '2020-08-15');
 
-SELECT * FROM hello;
+SELECT * FROM hello
 
-DATABASE DROP tumpa;
+-- jodi duitar akta match korle dibe
+-- WHERE lname = 'Sharma' OR dept='IT';
+
+-- jodi sob gulo match korte chai tahole AND use korbo
+-- WHERE lname = 'Sharma' AND dept='IT';
+
+-- ekta specific row dekhte chaile
+-- WHERE emp_id = 3;
+-- WHERE fname = 'Raji';
+
+-- limit kore dekhte chaile
+-- SELECT * FROM hello LIMIT 5 OFFSET 5;
+
+-- To delete a database
+-- DATABASE DROP tumpa;
+
+-- To LIKE operator
+-- WHERE fname LIKE 'R%';  -- R diye suru hoye je kono kichu
+-- WHERE fname LIKE '%n';  -- n diye ses hoye je kono kichu 
+-- WHERE fname LIKE '%u%';  -- jekono jaygay u thakle
+-- WHERE fname LIKE '_a%';  -- 2nd letter a hobe
+-- WHERE fname LIKE 'R%a';  -- R diye suru hobe and a diye ses hobe
+-- WHERE fname ILIKE 'r%';  -- case insensitive
+-- WHERE fname ILIKE '%A';  -- case insensitive
+-- WHERE fname ILIKE '%U%';  -- case insensitive
+-- WHERE fname ILIKE '_A%';  -- case insensitive
+-- WHERE fname ILIKE 'R___';  -- case insensitive
+-- WHERE fname ILIKE 'R%a';  -- case insensitive
+
+-- To filter with salary
+-- To update data
+UPDATE hello SET salary = 90000.00 WHERE emp_id = 3;
+UPDATE hello SET dept = 'HR' WHERE emp_id = 4;
+-- To delete data
+DELETE FROM hello WHERE emp_id = 3;
+DELETE FROM hello WHERE lname = 'Sharma' AND dept='IT';
+-- To drop table
+DROP TABLE das;
